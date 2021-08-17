94fe3d6a682525066b511ef871513bd1c45bc70e
Quixotic95 <e.oguz95@hotmail.com>
2021-08-17 18:15:19 +0300

    First commit.
    - spring initialized to project.
    
    - application.properties edited for db connection.
    
    - program runned without any error
    
    - gitignore files added.

---
+++ second-homework/src/test/java/dev/patika/quixotic95/secondhomework/SecondHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.quixotic95.secondhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
